04fede97c38c43c35db1eea6bd16a89f829a74a7
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 22:18:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,10 @@ require 'active_support/inflector'
 
 class Song
 
-  def self.table_name 
+  def self.table_name
     self.to_s.downcase.pluralize
-  end 
-  
+  end
+
   def self.column_names
     DB[:conn].results_as_hash = true
 
@@ -58,6 +58,3 @@ class Song
   end
 
 end
-
-
-
